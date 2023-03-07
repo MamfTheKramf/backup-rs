61d45c0b86ecebb5787cd1746243172fc2818dc2
Moritz Schrauth <moritzschrauth@gmail.com>
2023-03-07 21:11:21 +0100

    add next_backup field

--- config/src/profile_config.rs
+++ config/src/profile_config.rs
@@ -1,10 +1,15 @@
 //! Contains structs and functions for profile configurations.
 
-use std::{path::PathBuf, fs::{OpenOptions, File}, io::{Error, BufWriter, BufReader}};
+use std::{
+    fs::{File, OpenOptions},
+    io::{BufReader, BufWriter, Error},
+    path::PathBuf,
+};
 
+use crate::interval::*;
+use chrono::{offset, Days, NaiveDateTime};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
-use crate::interval::*;
 
 /// Struct representing a profile configuration.
 #[derive(Debug, Deserialize, Serialize)]
@@ -25,11 +30,14 @@ pub struct ProfileConfig {
     pub dirs_to_exclude: Vec<PathBuf>,
     /// Interval specifying when to make the next backup
     pub interval: Interval,
+    /// Datetime specifying when the next backup should be made
+    next_backup: NaiveDateTime,
 }
 
 impl ProfileConfig {
     /// Creates new [ProfileConfig] instance.
-    /// 
+    /// The `next_backup` field gets set to the creation time of this instance.
+    ///
     /// # Params
     /// - `name`: Name of the profile,
     /// - `target_dir`: Directory to place backup files in,
@@ -38,8 +46,18 @@ impl ProfileConfig {
     /// - `files_to_exclude`: List of files to exclude from backup,
     /// - `dirs_to_exclude`: List of dirs to exclude from backup,
     /// - `interval`: Interval specifying when to make the next backup.
-    pub fn new(name: String, target_dir: PathBuf, files_to_include: Vec<PathBuf>, dirs_to_include: Vec<PathBuf>, files_to_exclude: Vec<PathBuf>, dirs_to_exclude: Vec<PathBuf>, interval: Interval) -> ProfileConfig {
+    pub fn new(
+        name: String,
+        target_dir: PathBuf,
+        files_to_include: Vec<PathBuf>,
+        dirs_to_include: Vec<PathBuf>,
+        files_to_exclude: Vec<PathBuf>,
+        dirs_to_exclude: Vec<PathBuf>,
+        interval: Interval,
+    ) -> ProfileConfig {
         let uuid = Uuid::new_v4();
+        let now = offset::Local::now().naive_local();
+
         ProfileConfig {
             name,
             uuid,
@@ -48,12 +66,13 @@ impl ProfileConfig {
             dirs_to_include,
             files_to_exclude,
             dirs_to_exclude,
-            interval
+            interval,
+            next_backup: now,
         }
     }
 
     /// Attempts to load [ProfileConfig] from the given file.
-    /// 
+    ///
     /// # Returns
     /// [Ok] containing [ProfileConfig] if the file exists and is the correct format. [Error] else.
     pub fn load(file_path: &PathBuf) -> Result<ProfileConfig, Error> {
@@ -62,11 +81,6 @@ impl ProfileConfig {
 
         let config = serde_json::from_reader(reader)?;
         Ok(config)
-    }
-
-    /// Returns imutable reference to `uuid`
-    pub fn get_uuid(&self) -> &Uuid {
-        &self.uuid
     }
 
     /// Stores configuration to afile named after the own [Uuid] and places it into the directory pointed to by the given [PathBuf].
@@ -80,13 +94,50 @@ impl ProfileConfig {
 
         match serde_json::to_writer_pretty(writer, self) {
             Err(e) => Err(e.into()),
-            _ => Ok(())
+            _ => Ok(()),
         }
+    }
+
+    /// Returns immutable reference to `uuid`
+    pub fn get_uuid(&self) -> &Uuid {
+        &self.uuid
+    }
+
+    /// Returns immutable reference to `next_backup`.
+    /// `next_backup` isn't guaranteed to be matched by `interval. It must always be checked first.
+    pub fn next_backup(&self) -> &NaiveDateTime {
+        &self.next_backup
+    }
+
+    /// Updates the `next_backup` field according to `interval`.
+    /// The new values is always at least the current time.
+    ///
+    /// # Returns
+    /// Immutable reference to the `next_backup` field.
+    /// It isn't guaranteed, that `next_backup` is matched by `interval`. It must alwys be checked first.
+    pub fn set_next_backup(&mut self) -> &NaiveDateTime {
+        let now = offset::Local::now().naive_local();
+        if now >= self.next_backup {
+            self.next_backup = now;
+        }
+
+        match self.interval.next_datetime(self.next_backup) {
+            Some(datetime) => self.next_backup = datetime,
+            None => {
+                self.next_backup = self
+                    .next_backup
+                    .checked_add_days(Days::new(365))
+                    .unwrap_or(self.next_backup)
+            }
+        }
+
+        self.next_backup()
     }
 
     /// Converts a [PathBuf] describing a directory and a [Uuid] into a filename.
     fn dir_uuid_to_file(dir: &PathBuf, uuid: Uuid) -> PathBuf {
-        PathBuf::from(format!("{}/{}.json",
+        PathBuf::from(format!(
+            "{}/{}.json",
             dir.to_str().unwrap_or(""),
             uuid.as_hyphenated()
         ))
@@ -110,7 +161,15 @@ mod profile_config_tests {
         let name = "Hutzi".to_string();
         let target_file_dir = PathBuf::from("ho");
         let interval = IntervalBuilder::default().build().unwrap();
-        let config = ProfileConfig::new(name.clone(), target_file_dir.clone(), vec![], vec![], vec![], vec![], interval);
+        let config = ProfileConfig::new(
+            name.clone(),
+            target_file_dir.clone(),
+            vec![],
+            vec![],
+            vec![],
+            vec![],
+            interval,
+        );
         assert_eq!(config.name, name);
     }
 
@@ -120,7 +179,15 @@ mod profile_config_tests {
         let config_file_dir = PathBuf::from("test_tmp");
         let target_file_dir = PathBuf::from("ho");
         let interval = IntervalBuilder::default().build().unwrap();
-        let config = ProfileConfig::new(name.clone(), target_file_dir.clone(), vec![], vec![], vec![], vec![], interval);
+        let config = ProfileConfig::new(
+            name.clone(),
+            target_file_dir.clone(),
+            vec![],
+            vec![],
+            vec![],
+            vec![],
+            interval,
+        );
         assert!(config.store(&config_file_dir).is_ok());
         let file = ProfileConfig::dir_uuid_to_file(&config_file_dir, config.uuid);
         delete_file(file);
@@ -144,5 +211,65 @@ mod profile_config_tests {
         let file_path = ProfileConfig::dir_uuid_to_file(&dir_path, uuid);
         let config = ProfileConfig::load(&file_path);
         assert!(config.is_err());
+    }
+
+    mod set_next_backup_tests {
+        use chrono::{NaiveDate, Datelike};
+
+        use super::*;
+
+        #[test]
+        fn found_next_backup() {
+            let interval = IntervalBuilder::default()
+                .minutes(SpecifierKind::First)
+                .hours(SpecifierKind::First)
+                .monthdays(SpecifierKind::First)
+                .build()
+                .unwrap();
+            let mut p = ProfileConfig::new(
+                String::from("Hutzi"),
+                PathBuf::from("ho"),
+                vec![],
+                vec![],
+                vec![],
+                vec![],
+                interval,
+            );
+            let now = offset::Local::now().naive_local();
+            let next_month = now.month() % 12 + 1;
+            let next_year = now.year() + now.month() as i32 / 12;
+            let next_match = NaiveDate::from_ymd_opt(next_year, next_month, 1).unwrap()
+                .and_hms_opt(0, 0, 0).unwrap();
+            assert_eq!(p.set_next_backup(), &next_match);
+
+            p.next_backup = NaiveDate::from_ymd_opt(2000, 12, 16).unwrap()
+                .and_hms_opt(12, 30, 6).unwrap();
+            let now = offset::Local::now().naive_local();
+            let next_month = now.month() % 12 + 1;
+            let next_year = now.year() + now.month() as i32 / 12;
+            let next_match = NaiveDate::from_ymd_opt(next_year, next_month, 1).unwrap()
+                .and_hms_opt(0, 0, 0).unwrap();
+            assert_eq!(p.set_next_backup(), &next_match);
+        }
+
+        #[test]
+        fn not_found_next_backup() {
+            let interval = IntervalBuilder::default()
+                .minutes(SpecifierKind::None)
+                .build()
+                .unwrap();
+            let mut p = ProfileConfig::new(
+                String::from("Hutzi"),
+                PathBuf::from("ho"),
+                vec![],
+                vec![],
+                vec![],
+                vec![],
+                interval,
+            );
+
+            let now = offset::Local::now().naive_local();
+            assert!(&now <= p.set_next_backup());
+        }
     }
 }
