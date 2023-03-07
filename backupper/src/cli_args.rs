3191c858a7f5cf53d5bf8de520fb034b19fcd289
Moritz Schrauth <moritzschrauth@gmail.com>
2023-03-07 21:41:03 +0100

    add clap

---
+++ backupper/src/cli_args.rs
@@ -0,0 +1,25 @@
+use clap::{ ArgGroup, Parser };
+
+/// Crate for creating backups.
+/// Allows to only check for specific Backup Profiles if either the name or the uuid are provided, or checks all of them, if nothing is provided.
+#[derive(Parser)]
+#[command(author, version, about, long_about)]
+#[command(group(
+    ArgGroup::new("id")
+        .required(true)
+        .args(["name", "uuid"]),
+))]
+pub struct Args {
+    /// Name of Profile to check.
+    #[arg(short, long)]
+    name: Option<String>,
+
+    /// Uuid of Profile to check.
+    #[arg(short, long)]
+    uuid: Option<String>,
+}
+
+/// Parses cli-args and returns them.
+pub fn get_args() -> Args {
+    Args::parse()
+}
--- backupper/src/main.rs
+++ backupper/src/main.rs
@@ -1,8 +1,10 @@
+mod cli_args;
 mod config;
 
 use std::env;
 
 fn main() {
+    let args = cli_args::get_args();
     println!("{:?}", env::current_dir());
     println!("{:?}", config::load_general_config());
 }
