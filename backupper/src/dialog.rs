49e44af5c206b9958ba369e0b4e5265a4596a8a4
Moritz Schrauth <moritzschrauth@gmail.com>
2023-03-18 14:25:44 +0100

    fix dialog strings

--- backupper/src/dialog.rs
+++ backupper/src/dialog.rs
@@ -10,8 +10,8 @@ fn as_u16_vec(text: &str) -> Vec<u16> {
 
 /// Opens a Retry-Message box with the given parameters and returns the users answer.
 pub fn retry_dialog(title: &str, msg: &str, styles: MESSAGEBOX_STYLE) -> MESSAGEBOX_RESULT {
-    let title = as_u16_vec(title);
-    let msg: Vec<u16> = as_u16_vec(msg);
+    let title = as_u16_vec(&format!("{}\0", title));
+    let msg: Vec<u16> = as_u16_vec(&format!("{}\0", msg));
     unsafe {
         MessageBoxW(None, PCWSTR::from_raw(msg.as_ptr()), PCWSTR::from_raw(title.as_ptr()), MB_RETRYCANCEL | styles)
     }
@@ -19,8 +19,8 @@ pub fn retry_dialog(title: &str, msg: &str, styles: MESSAGEBOX_STYLE) -> MESSAGE
 
 /// Opens an Info-Message box wiht the given parameters and returns the users answer.
 pub fn info_dialog(title: &str, msg: &str, styles: MESSAGEBOX_STYLE) -> MESSAGEBOX_RESULT {
-    let title = as_u16_vec(title);
-    let msg: Vec<u16> = as_u16_vec(msg);
+    let title = as_u16_vec(&format!("{}\0", title));
+    let msg: Vec<u16> = as_u16_vec(&format!("{}\0", msg));
     unsafe {
         MessageBoxW(None, PCWSTR::from_raw(msg.as_ptr()), PCWSTR::from_raw(title.as_ptr()), MB_OK | MB_ICONINFORMATION | styles)
     }
