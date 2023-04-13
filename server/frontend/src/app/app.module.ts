a924f1c26789a80580397ec6197311391d8be400
Moritz Schrauth <moritzschrauth@gmail.com>
2023-04-13 13:01:06 +0200

    generate side-nav

--- server/frontend/src/app/app.module.ts
+++ server/frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule
